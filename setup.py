f6c237a44644d181ec627237ac704118b373fb92
Christopher Harrison <ch12@sanger.ac.uk>
2016-03-10 11:23:10 +0000

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name    = 'Ypip',
+    version = '0.0.1',
+    license = 'MIT',
+
+    description = 'Recursive pip for VCS-based packages',
+    long_description = open().read('README.rst')
+)
