4c2bdb28cf787e252f3e28da8985cf9b7c12b3ce
Christopher Harrison <ch12@sanger.ac.uk>
2016-04-29 15:25:08 +0100

    Trixes

--- ypip/sources/pep440/specifier.py
+++ ypip/sources/pep440/specifier.py
@@ -23,27 +23,34 @@ from ypip.sources.pep440.exceptions import ParseError
 _PredicateT = Callable[[Version], bool]
 
 def _equality_factory(rhs:Version, wildcard:bool) -> _PredicateT:
+    # TODO
     pass
 
 def _inequality_factory(rhs:Version, wildcard:bool) -> _PredicateT:
-    pass
+    eq = _equality_factory(rhs, wildcard)
+    return lambda lhs: not eq(lhs)
 
 def _absolute_factory(rhs:Version) -> _PredicateT:
-    pass
+    return lambda lhs: lhs == rhs
 
 def _lt_factory(rhs:Version) -> _PredicateT:
-    pass
+    return lambda lhs: lhs < rhs
 
 def _lte_factory(rhs:Version) -> _PredicateT:
-    pass
+    eq = _equality_factory(rhs, False)
+    lt = _lt_factory(rhs)
+    return lambda lhs: eq(lhs) or lt(lhs)
 
 def _gt_factory(rhs:Version) -> _PredicateT:
-    pass
+    lte = _lte_factory(rhs)
+    return lambda lhs: not lte(lhs)
 
 def _gte_factory(rhs:Version) -> _PredicateT:
-    pass
+    lt = _lt_factory(rhs)
+    return lambda lhs: not lt(lhs)
 
 def _compatible_factory(rhs:Version) -> _PredicateT:
+    # TODO
     pass
 
 
@@ -61,15 +68,15 @@ class Specifier(object):
 
     wildcard = re.compile(r'\.?\d*\*')
 
-    factory = {
+    predicate_factory = {
         '==':  _equality_factory,
         '!=':  _inequality_factory,
-        '===': lambda v, _: _absolute_factory(v),
-        '<':   lambda v, _: _lt_factory(v),
-        '<=':  lambda v, _: _lte_factory(v),
-        '>':   lambda v, _: _gt_factory(v),
-        '>=':  lambda v, _: _gte_factory(v),
-        '~=':  lambda v, _: _compatible_factory(v)
+        '===': lambda rhs, _: _absolute_factory(rhs),
+        '<':   lambda rhs, _: _lt_factory(rhs),
+        '<=':  lambda rhs, _: _lte_factory(rhs),
+        '>':   lambda rhs, _: _gt_factory(rhs),
+        '>=':  lambda rhs, _: _gte_factory(rhs),
+        '~=':  lambda rhs, _: _compatible_factory(rhs)
     }
 
     def __init__(self, spec:str):
@@ -102,7 +109,7 @@ class Specifier(object):
             version = Version(v)
 
             # Add comparator predicate to conjunction
-            self.comparators.append(Specifier.factory[op](version, has_wildcard))
+            self.comparators.append(Specifier.predicate_factory[op](version, has_wildcard))
 
     def __call__(self, version:Version) -> bool:
         """
